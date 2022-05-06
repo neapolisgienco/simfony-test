eaa44032bf8515f6f1e00033a1e7fd87ea0d806f
neapolis <neapolis@inbox.ru>
2022-05-06 11:05:12 +0200

    выведение дампов таблиц базы данных на страницы бейз нтмл твиг не менялся - отображение автоматом из контроллера

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $products =$this ->getDoctrine() ->getRepository('AppBundle:Product') ->findAll();
+        dump($products);
+        $products2 =$this ->getDoctrine() ->getRepository('AppBundle:Product2') ->findAll();
+        dump($products2);
+
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
