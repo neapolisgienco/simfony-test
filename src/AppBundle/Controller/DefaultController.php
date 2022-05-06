7e13316fb9c0869a2e76e69d30e88c520f812126
neapolis <neapolis@inbox.ru>
2022-05-06 13:32:40 +0200

    добавлен вывод значений из базы в цикле на страницу

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,14 +18,14 @@ class DefaultController extends Controller
         $products2 =$this ->getDoctrine() ->getRepository('AppBundle:Product2') ->findAll();
         dump($products2);
 
-        $products=[];
+        $productsMassiv=[];
         for ($i=1; $i <= 10; $i++){//урок 6 2.40
-            $products[] =rand(1, 100);
+            $productsMassiv[] =rand(1, 100);
         }
 
 
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', ['products' => $products,
+        return $this->render('default/index.html.twig', ['productsMassiv' => $productsMassiv, 'products' => $products,
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
